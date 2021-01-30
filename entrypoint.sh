701dc6ae8ede65cbcebe9723c6d72722dfbd1cc0
mib <mib@mib.dev>
2021-01-30 16:12:55 +0100

    Make actually format code and not just check style

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,6 @@ cd "$GITHUB_WORKSPACE"
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 DEFAULT_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
 
-RED="\u001b[31m"
-GREEN="\u001b[32m"
-RESET="\u001b[0m"
-
 # Maintain support for uncrustify's ENV variable if it's passed in 
 # from the actions file. Otherwise the actions file could have the
 # configPath argument set
@@ -27,28 +23,12 @@ else
     CONFIG=" -c /default.cfg"
 fi
 
-EXIT_VAL=0
-
 while read -r FILENAME; do
-    TMPFILE="${FILENAME}.tmp"
-    # Failure is passed to stderr so we need to redirect that to grep so we can pretty print some useful output instead of the deafult
-    # Success is passed to stdout, so we need to redirect that separately so we can capture either case.
-
-    # Allow failures here so we can capture exit status
-    set +e
-    OUT=$(uncrustify --check${CONFIG} -f ${FILENAME} -l CS 2>&1 | grep -e ^FAIL -e ^PASS | awk '{ print $2 }'; exit ${PIPESTATUS[0]})
-    RETURN_VAL=$?
-
-    # Stop allowing failures again
-    set -e 
-
-    if [[ $RETURN_VAL -gt 0 ]]; then
-        echo -e "${RED}${OUT} failed style checks.${RESET}"
-        uncrustify${CONFIG} -f ${FILENAME} -o ${TMPFILE} && colordiff -u ${FILENAME} ${TMPFILE}
-        EXIT_VAL=$RETURN_VAL
-    else
-        echo -e "${GREEN}${OUT} passed style checks.${RESET}"
-    fi
+    uncrustify ${CONFIG} -f ${FILENAME} -l CS -o ${FILENAME}
 done < <(git diff --name-status --diff-filter=AM origin/${DEFAULT_BRANCH}...${BRANCH_NAME} -- '*.cs' | awk '{ print $2 }' )
 
-exit $EXIT_VAL
+echo "Config: ${CONFIG}"
+echo "Changed files:"
+git diff --name-status --diff-filter=AM origin/${DEFAULT_BRANCH}...${BRANCH_NAME} -- '*.cs' | awk '{ print $2 }'
+
+exit 0
